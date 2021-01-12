49cab6f43c72f593446a10d4f943cb4978278309
Emmanuel KWENE <njume48@gmail.com>
2021-01-12 18:43:31 +0100

    Removes build files from git

--- app/Views/about.php
+++ app/Views/about.php
@@ -2,7 +2,20 @@
 
 
 <div id="page" class="about-page page" data-key="about">
-
-	<h1>Qui sommes-nous?</h1>
-
+	<div class="hero">
+		<div class="hero__bg">
+			<img class="hero__image" src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="Comprendre la culture, les mentalités et le monde des affaires en Afrique centrale est primordial pour reussir.">
+		</div>
+		<div class="hero__content">
+			<h1 class="hero__title">
+				Qui sommes-nous?
+			</h1>
+			<div class="hero__copy">
+				E&A Business Consulting vous accompagne à toutes les étapes du développement de votre business en Afrique centrale.
+			</div>
+		</div>
+	</div>
+	<br><br><br><br>
+	<section class="section"></section>
+	<section class="section"></section>
 </div>
