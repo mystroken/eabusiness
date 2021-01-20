b03ad5c1809823c07cd9fed0e6d02fa8d0e988df
Asam237 <abbasalimokolo@gmail.com>
2021-01-20 23:53:38 +0100

    Conception de la page

--- app/Views/partners.php
+++ app/Views/partners.php
@@ -3,5 +3,23 @@
 $this->layout('layouts/base', []) ?>
 
 <div id="page" class="partners-page page" data-key="partners">
-    <p>Hello</p>
+    <div class="section">
+        <section class="section__content">
+            <h1 class="section__title">
+                Un carnet de consultants ayant fait leurs preuves
+            </h1>
+            <div class="section__subtitle">
+                E&A Business Consulting dispose d’une liste de référence de partenaires de choix dont l’expertise pourrait être nécessaire pendant l’exécution de certains de nos mandats. Nous avons travaillé avec eux à de maintes occasions pour le compte de nos clients et ils ont fait leurs preuves. Il s’agit de :
+            </div>
+            <div class="section__liste">
+            — Stratégistes et lobbyiste corporatifs <br>
+            — Juristes en droit international des affaires <br>
+            — Comptables et auditeurs <br>
+            — Experts en financiers et en levée de financement <br>
+            — Experts en fusion et acquisitions de sociétés <br>
+            — Consultants en santé et securité de travaille <br>
+            — Conseillers en gestion de la sécurité <br>
+            </div>
+        </section>
+    </div>
 </div>
