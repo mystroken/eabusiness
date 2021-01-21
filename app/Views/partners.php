84e1afbcd2a73dfb1bfd0b66f561fe4308c7950b
Asam237 <abbasalimokolo@gmail.com>
2021-01-21 06:20:58 +0100

    Ajout de hero

--- app/Views/partners.php
+++ app/Views/partners.php
@@ -3,15 +3,27 @@
 $this->layout('layouts/base', []) ?>
 
 <div id="page" class="partners-page page" data-key="partners">
-    <div class="section">
-        <section class="section__content">
-            <h1 class="section__title">
+    <div class="">
+    	<div class="hero">
+		    <div class="hero__bg">
+		    	<img class="hero__image" src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="Comprendre la culture, les mentalités et le monde des affaires en Afrique centrale est primordial pour reussir.">
+		    </div>
+		    <div class="hero__content">
+		    	<h1 class="hero__title">
+		    		Nos partenaires
+		    	</h1>
+		    </div>
+	    </div>
+	<br><br><br><br>
+     <div class="section">
+         <section class="section__content">
+             <h1 class="section__title">
                 Un carnet de consultants ayant fait leurs preuves
-            </h1>
-            <div class="section__subtitle">
+             </h1>
+             <div class="section__subtitle">
                 E&A Business Consulting dispose d’une liste de référence de partenaires de choix dont l’expertise pourrait être nécessaire pendant l’exécution de certains de nos mandats. Nous avons travaillé avec eux à de maintes occasions pour le compte de nos clients et ils ont fait leurs preuves. Il s’agit de :
-            </div>
-            <div class="section__liste">
+             </div>
+             <div class="section__liste">
             — Stratégistes et lobbyiste corporatifs <br>
             — Juristes en droit international des affaires <br>
             — Comptables et auditeurs <br>
@@ -19,7 +31,24 @@ $this->layout('layouts/base', []) ?>
             — Experts en fusion et acquisitions de sociétés <br>
             — Consultants en santé et securité de travaille <br>
             — Conseillers en gestion de la sécurité <br>
-            </div>
-        </section>
+             </div>
+         </section>
+     </div>
+     <div class="section">
+         <section class="section__paterns">
+             <h1 class="section__title">
+             Ils nous font confiance
+             </h1>
+             <div class="section__pictures mx-auto">
+                <div class="row flex">
+                    <div class="col-md-3 col-lg-3">
+                        <img src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="" class="img-fluid">
+                        <img src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="" class="img-fluid">
+                        <img src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="" class="img-fluid">
+                    </div>
+                </div>
+             </div>
+         </section>
+     </div>
     </div>
 </div>
