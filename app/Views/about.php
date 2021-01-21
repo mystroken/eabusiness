bcb502d70a3b129594a5106a37ad216f0af50234
Asam237 <abbasalimokolo@gmail.com>
2021-01-21 18:59:57 +0100

    HTML Struture reviewed #3

--- app/Views/about.php
+++ app/Views/about.php
@@ -2,7 +2,7 @@
 
 
 <div id="page" class="about-page page" data-key="about">
-	<div class="hero">
+	<div class="hero centered">
 		<div class="hero__bg">
 			<img class="hero__image" src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="Comprendre la culture, les mentalités et le monde des affaires en Afrique centrale est primordial pour reussir.">
 		</div>
@@ -11,11 +11,34 @@
 				Qui sommes-nous?
 			</h1>
 			<div class="hero__copy">
-				E&A Business Consulting vous accompagne à toutes les étapes du développement de votre business en Afrique centrale.
+				En savoir plus sur le cabinet d’études et de conseils   E&A Business Consulting.
 			</div>
 		</div>
 	</div>
-	<br><br><br><br>
-	<section class="section"></section>
-	<section class="section"></section>
+	<section class="section">
+		<div class="section__inner">
+			<div class="content">
+				<h1 class="content__title">
+					Un cabinet d'études et de conseils en affaires
+				</h1>
+				<p class="content__body">
+					<b>E&A Business Consulting</b> est un cabinet d'études et de conseils en affaires avec pour focus l’Afrique centrale et en programmes de développement qui <b>appuie</b> et <b>conseille</b> les entreprises privées qui souhaitent entrer sur <b>le marché africain.</b> <br><br>
+					Il vient également en aide aux gouvernements locaux, régionaux et nationaux dans l’ <b> élaboration de stratégies de développement économique</b> qui améliorent leur environnement commercial et leur attractivité pour les investissements étrangers.
+				</p>
+				<h4 class="content__subtitle">Nos atouts</h4>
+				<p class="content__body">
+					E&A Business Consulting vous offre un ensemble d'atouts avantageux pour commencer du bon pied en Afrique Subsaharienne et transformer votre projet d'implémentation en une réalité créatrice de valeurs, dans les meilleures conditions et les plus brefs délais.
+				</p>
+				<h4 class="content__subtitle">Nos valeurs</h4>
+				<p class="content__body">
+					E&A Business Consulting développe une culture d'entreprise centrée sur la satisfaction de ses clients et de ses collaborateurs et sur l'atteinte des objectifs fixés dans le respect de valeurs de Qualité et d'Excellence.
+				</p>
+				<h4 class="content__subtitle">
+					Notre mission
+				</h4>
+				<p class="content__body">
+					Apporter notre expertise et notre connaissance intime du marché africain et de sa culture d’affaires au service de nos clients afin de les accompagner efficacement dans l’exploration et la mise en œuvre de nouvelles occasions d’affaires et investissements.
+				</p>
+			</div>
+		</div>
 </div>
