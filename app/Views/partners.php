157e8c4d86be177de0a678648d69e85169725767
Emmanuel KWENE <njume48@gmail.com>
2021-01-21 07:08:25 +0100

    HTML Struture reviewed

--- app/Views/partners.php
+++ app/Views/partners.php
@@ -3,50 +3,59 @@
 $this->layout('layouts/base', []) ?>
 
 <div id="page" class="partners-page page" data-key="partners">
-    <div class="hero">
-	  <div class="hero__bg">
-             <img class="hero__image" src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="Comprendre la culture, les mentalités et le monde des affaires en Afrique centrale est primordial pour reussir.">
+	<div class="hero centered">
+		<div class="hero__bg">
+			<img class="hero__image" src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg"
+				alt="Comprendre la culture, les mentalités et le monde des affaires en Afrique centrale est primordial pour reussir.">
 		</div>
 		<div class="hero__content">
-		   	<h1 class="hero__title">
-		   		Nos partenaires
-		   	</h1>
+			<h1 class="hero__title">Nos partenaires</h1>
+			<div class="hero__copy">
+				Une question? Un conseil? Nous sommes disposés à répondre à chacune de vos préoccupations.
+			</div>
 		</div>
-     </div>
-     <div class="section">
-         <section class="section__content">
-             <h1 class="section__title">
-                Un carnet de consultants ayant fait leurs preuves
-             </h1>
-             <div class="section__subtitle">
-                E&A Business Consulting dispose d’une liste de référence de partenaires de choix dont l’expertise pourrait être nécessaire pendant l’exécution de certains de nos mandats. Nous avons travaillé avec eux à de maintes occasions pour le compte de nos clients et ils ont fait leurs preuves. Il s’agit de :
-             </div>
-             <div class="section__liste">
-            — Stratégistes et lobbyiste corporatifs <br>
-            — Juristes en droit international des affaires <br>
-            — Comptables et auditeurs <br>
-            — Experts en financiers et en levée de financement <br>
-            — Experts en fusion et acquisitions de sociétés <br>
-            — Consultants en santé et securité de travaille <br>
-            — Conseillers en gestion de la sécurité <br>
-             </div>
-         </section>
-     </div>
-     <div class="section">
-         <section class="section__paterns">
-             <h1 class="section__title">
-             Ils nous font confiance
-             </h1>
-             <div class="section__pictures mx-auto">
-                <div class="row flex">
-                    <div class="col-md-3 col-lg-3">
-                        <img src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="" class="img-fluid">
-                        <img src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="" class="img-fluid">
-                        <img src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="" class="img-fluid">
-                    </div>
-                </div>
-             </div>
-         </section>
-     </div>
-    </div>
+	</div>
+
+	<section class="section">
+		<div class="section__inner">
+			<div class="content">
+				<h1 class="content__title">
+					Un carnet de consultants ayant fait leurs preuves
+				</h1>
+				<div class="content__body">
+					<p>E&A Business Consulting dispose d’une liste de référence de partenaires de choix dont l’expertise
+						pourrait être nécessaire pendant l’exécution de certains de nos mandats. Nous avons travaillé avec eux à
+						de maintes occasions pour le compte de nos clients et ils ont fait leurs preuves. Il s’agit de :
+					</p>
+					<ul>
+						<li>Stratégistes et lobbyiste corporatifs</li>
+						<li>Juristes en droit international des affaires</li>
+						<li>Comptables et auditeurs</li>
+						<li>Experts en financiers et en levée de financement</li>
+						<li>Experts en fusion et acquisitions de sociétés</li>
+						<li>Consultants en santé et securité de travaille</li>
+						<li>Conseillers en gestion de la sécurité</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="section">
+		<div class="section__inner">
+			<div class="content">
+				<h1 class="content__title">
+					Ils nous font confiance
+				</h1>
+				<div class="content__body">
+					<div class="d-flex">
+						<img src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="">
+						<img src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="">
+						<img src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
 </div>
