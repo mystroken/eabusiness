73b1e431303174a5b80ce7abc9242fb1f10e51b3
Emmanuel KWENE <njume48@gmail.com>
2021-01-27 13:44:06 +0100

    :wrench: Events page started

--- app/Views/events.php
+++ app/Views/events.php
@@ -5,7 +5,7 @@ $this->layout('layouts/base', ['title' => 'Nos Évènements']) ?>
 <div id="page" class="events-page page" data-key="events">
 	<div class="hero centered">
 		<div class="hero__bg">
-			<img class="hero__image" src="/img/caroussel/eabusiness_caroussel_cover-1.jpeg" alt="Nos évènements">
+			<img class="hero__image" src="/img/cover-events-page.jpg" alt="Nos évènements">
 		</div>
 		<div class="hero__content">
 			<h1 class="hero__title">Nos évènements</h1>
@@ -13,4 +13,14 @@ $this->layout('layouts/base', ['title' => 'Nos Évènements']) ?>
 		</div>
 	</div>
 
+	<section class="section">
+		<div class="section__inner">
+			<div class="content">
+				<h1 class="content__title">Organisation des missions économiques & B2B</h1>
+				<div class="content__body">
+					<p>Nous organisons les missions économiques qui visent, d’une part, à promouvoir l’Afrique Centrale en tant que terre d’accueil pour des entreprises ainsi qu’à attirer des investissements et, d’autre part, à soutenir les entreprises exportatrices (ou non)dans leur conquête de marchés étrangers. Les missions permettent ainsi d’attirer des investissements vers l'Afrique Centrale et d’ouvrir la voie à de nouvelles opportunités d’affaires. Non seulement que nous organisons des missions économiques, nous organisons aussi des rencontres d'affaires B2B.</p>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
