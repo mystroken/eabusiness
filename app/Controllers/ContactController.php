fca1cc2864568ff179ef65854362ab996fbd7ae1
Asam237 <abbasalimokolo@gmail.com>
2021-02-09 21:01:14 +0100

    Send Mail

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -8,9 +8,33 @@ use App\Controller;
 
 class ContactController extends Controller
 {
+
 	public function index(): void
 	{
-		echo $this->templates->render('contact');
+		$sendMail = $this->sendMail() ;
+		echo $this->templates->render('contact',[
+			'sendMail' => $sendMail,
+		] );
+	}
+
+
+	public function sendMail()
+	{
+		if(isset($_POST['email']) && $_POST['email'] !='')
+		{
+			if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+			{
+				$name = $_POST['name'];
+				$email = $_POST['email'];
+				$message = $_POST['message'];
+				$to ="aaa@gmail.com";
+				mail($to, $email, $message);
+			}
+		}
+		else
+		{
+			echo "champ de validation incorret !";
+		}
 	}
 
 }
