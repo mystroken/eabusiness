8367e4355adb247179fba666dedc5530b8e9f756
Emmanuel KWENE <njume48@gmail.com>
2021-02-10 23:24:32 +0100

    :wrench: Try to add phpmailer

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -28,7 +28,14 @@ class ContactController extends Controller
 				$to ="mystroken@gmail.com";
 				$subject = "Message depuis eabusiness.africa";
 				$message = "Voici un message de test pour votre jolie et belle boite mail monsieur, madame.";
-				mail($to, $subject, $message);
+				$headers = 'From: webmaster@example.com' . "\r\n" .
+				'Reply-To: webmaster@example.com' . "\r\n" .
+				'X-Mailer: PHP/' . phpversion();
+				try {
+					mail($to, $subject, $message, $headers);
+				} catch (\Exception $e) {
+					echo $e->getMessage();
+				}
 				echo "Mail envoyé !";
 			}
 		}
@@ -37,7 +44,39 @@ class ContactController extends Controller
 			echo "champ de validation incorret !";
 		}
 
-		echo $this->templates->render('contact');
+// 		$apiKey = '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';
+// //		$mailer = new Sendgrid($apiKey);
+// //
+// //		$email = (new Email())
+// //			->addTo('mystroken@gmail.com')
+// //			->setFrom('noreply@eabusiness.africa')
+// //			->setSubject('Hello, world!')
+// //			->setTextBody('Hello World! How are you?');
+// //
+// //		try {
+// //			$mailer->send($email);
+// //		} catch (Exception $e) {
+// //			echo $e->getMessage();
+// //		}
+// 		$email = new \SendGrid\Mail\Mail();
+// 		$email->setFrom("mystroken@gmail.com", "Mystro Ken");
+// 		$email->setSubject("Sending with Twilio SendGrid is Fun");
+// 		$email->addTo("emmanuel.kwene@flexyla.com", "Mystro Ken");
+// 		$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+// 		$email->addContent(
+// 			"text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+// 		);
+// 		$sendgrid = new \SendGrid($apiKey);
+// 		try {
+// 			$response = $sendgrid->send($email);
+// 			print $response->statusCode() . "\n";
+// 			print_r($response->headers());
+// 			print $response->body() . "\n";
+// 		} catch (\Exception $e) {
+// 			echo 'Caught exception: '. $e->getMessage() ."\n";
+// 		}
+
+//		echo $this->templates->render('contact');
 	}
 
 }
