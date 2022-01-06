2fee66309bc84919ea7f349e9f1bb482b5c06051
Emmanuel KWENE <mystroken@gmail.com>
2022-01-06 12:26:27 +0100

    About & Contact page updated

--- app/Views/about.php
+++ app/Views/about.php
@@ -1,4 +1,4 @@
-<?php $this->layout('layouts/base', ['title' => 'A Propos du cabinet E&A Business Consulting']) ?>
+<?php $this->layout('layouts/base', ['title' => 'A Propos du cabinet A&E Business Consulting']) ?>
 
 
 <div id="page" class="about-page page" data-key="about">
@@ -11,7 +11,7 @@
 				Qui sommes-nous?
 			</h1>
 			<div class="hero__copy">
-				En savoir plus sur le cabinet d’études et de conseils   E&A Business Consulting.
+				En savoir plus sur le cabinet d’études et de conseils A&E Business Consulting.
 			</div>
 		</div>
 	</div>
@@ -22,7 +22,7 @@
 					Un Cabinet conseils en affaires
 				</h1>
 				<p class="content__body">
-					E&A Business Consulting Sarl est un cabinet conseils en affaires tourné vers l’Afrique, qui développe, appuie et conseil les entreprises qui souhaitent faire des affaires en Afrique.
+					A&E Business Consulting Sarl est un cabinet conseils en affaires tourné vers l’Afrique, qui développe, appuie et conseil les entreprises qui souhaitent faire des affaires en Afrique.
 				<br><br>
 					Notre Cabinet accompagne également les autorités locales dans l’élaboration des stratégies de développement économique, en vue de l’amélioration de leur environnement commercial, et leur attractivité pour les investissements étrangers.
 				</p>
@@ -30,7 +30,7 @@
 			<div class="content">
 				<h4 class="content__title">Nos atouts</h4>
 				<p class="content__body">
-					E&A Business Consulting Sarl vous offre un ensemble de services et prestations qui vous permettent d’affronter en toute sérénité le marché africain, et de concrétiser votre projet d'implémentation en une réalité créatrice de valeurs.<br>
+					A&E Business Consulting Sarl vous offre un ensemble de services et prestations qui vous permettent d’affronter en toute sérénité le marché africain, et de concrétiser votre projet d'implémentation en une réalité créatrice de valeurs.<br>
 					<a class="" href="<?= url('expertise'); ?>">
 						En savoir plus sur nos service &rarr;
 					</a>
@@ -50,12 +50,12 @@
 				<div class="content">
 					<h1 class="content__title">Notre Cabinet</h1>
 					<p class="content__body">
-						E&A Business Consulting Sarl est une société basée au <strong>Cameroun</strong> avec une réprésentation en <strong>Autriche</strong>, nous sommes trilingues (anglais, français et allemand) avec une portée continentale.<br>
-						Avec une proximité culturelle, des liens étroits et bilatéraux avec l’Afrique, notre réseau de contacts clés sur le continent et une pléiade de professionnels hautement qualifiés et bilingues, E&A Business Consulting Sarl est particulièrement bien placée pour être<strong> votre conseiller</strong>, <strong>votre guide</strong> et <strong>votre facilitateur</strong>.
+						A&E Business Consulting Sarl est une société basée au <strong>Cameroun</strong> avec une réprésentation en <strong>Autriche</strong>, nous sommes trilingues (anglais, français et allemand) avec une portée continentale.<br>
+						Avec une proximité culturelle, des liens étroits et bilatéraux avec l’Afrique, notre réseau de contacts clés sur le continent et une pléiade de professionnels hautement qualifiés et bilingues, A&E Business Consulting Sarl est particulièrement bien placée pour être<strong> votre conseiller</strong>, <strong>votre guide</strong> et <strong>votre facilitateur</strong>.
 					</p>
 				</div>
 				<figure class="about-section__figure">
-					<img src="/img/cover-contact-page.jpg" alt="E&A Business Consulting" class="about-section__img">
+					<img src="/img/cover-contact-page.jpg" alt="A&E Business Consulting" class="about-section__img">
 				</figure>
 			</div>
 		</div>
